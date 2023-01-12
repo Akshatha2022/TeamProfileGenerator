5d6e5bba57c8c69c8e9111edf96e690b7737532a
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-01-11 19:20:15 -0600

    passing all 4 tests from commenting out conditions

--- index.js
+++ index.js
@@ -1,70 +1,132 @@
+// node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeam = require("./src/page-template.js");
-const team = [];
+
+// lib modules
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const questions = [
-  {
-    type: "list",
-    message: "What is your role?",
-    choices: ['Intern','Engineer', 'Manager', 'Employee']
-  },
-  {
-    type: "input",
-    message: "What is your name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your ID number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your email?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What university did you attend?",
-    name: "school",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub user name?",
-    name: "github",
-  }
-];
-// TODO: Create a function to write to html, will place this in function init
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-      err ? console.log(err) : console.log("Success!")
-    );
-  }
+// Array for answers to questions
+const newStaffMemberData = [];
+
+// Array object questions asked in CLI to user
+const questions = async () => {
+  const answers = await inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+    ])
+
+
+    
+    //  console.log(answers);
+      // if manager selected, answer these specific question
+      if (answers.role === "Manager") {
+        const managerAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your office number",
+              name: "officeNumber",
+            },
+          ])
+          const newManager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+          );
+          newStaffMemberData.push(newManager);
+          
+        // if engineer selected answer these set of questions
+      } else if (answers.role === "Engineer") {
+        const githubAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your GitHub user name?",
+              name: "github",
+            }
+          ])
+            const newEngineer = new Engineer(
+              answers.name,
+              answers.id,
+              answers.email,
+              githubAns.github
+            );
+            newStaffMemberData.push(newEngineer);
+          
+        // if intern selected answer these set of questions
+      } else if (answers.role === "Intern") {
+        const internAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What university did you attend?",
+              name: "school",
+            },
+          ])
+          
+          const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.school
+          );
+          newStaffMemberData.push(newIntern);          
+      } 
+
+}; //end of questions function
+
+async function promptQuestions() {
+  await questions()
+    
   
-  // TODO: Create a function to initialize app
-  function init() {
-    // prompt method calls in array of questions
-    inquirer
-      .prompt(questions)
-      // promise, answers function takes answer responses and writes to readme
-      .then((answers) => {
-        console.log(answers);
-        const response = pageTemplate(answers);
-        writeToFile("index.html", response);
+  const addMemberAns = await inquirer
+    .prompt([
+      {
+        name:'addMember',
+        type: 'list',
+        choices: ['Add a new member', 'Create team'],
+        message: "What would you like to do next?"
+      }
+    ])
+
+    if (addMemberAns.addMember === 'Add a new member') {
+      return promptQuestions()
     }
-    function manager() {
-        inquirer.prompt(questions)
-          .then((data) => {
-            cosnt newManager = new Manager(data.name, data.id, data.email, data.officeNumber)
-            team.push(newManager);
-          });
-    // Function call to initialize app
-    init();
+    return createTeam();
+}  
+
+promptQuestions();
+
+function createTeam () {
+  console.log("new guy", newStaffMemberData)
+  fs.writeFileSync(
+    "./output/index.html",
+    generateTeam(newStaffMemberData),
+    "utf-8"
+  );
 }
 
-
-//     fs.writeFile("index.html", generateTeam(data), (err) =>
-//        err ? console.log(err) : console.log('Success!')
-//  );
